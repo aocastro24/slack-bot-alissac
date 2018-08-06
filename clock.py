7610f2d491037bad3291c3e90aa1dca9ea29b7c9
Alissa Castro (IS-PH) <alissa_castro@trendmicro.com>
2018-08-07 00:37:27 +0800

    Finalization of clock scheduler

--- clock.py
+++ clock.py
@@ -4,22 +4,20 @@ from hello.views import slack_bToken, trendingPosts
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    slack_bToken.api_call(
-        "chat.postMessage",
-        channel="CC4A68V54",
-        text=trendingPosts()
-    )
-
-
-# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=0)
-# def scheduled_job():
-#     # views.postTrend("CC4A68V54", views.trendingPosts())
+# @sched.scheduled_job('interval', minutes=1)
+# def timed_job():
 #     slack_bToken.api_call(
 #         "chat.postMessage",
 #         channel="CC4A68V54",
 #         text=trendingPosts()
 #     )
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=13)
+def scheduled_job():
+    slack_bToken.api_call(
+        "chat.postMessage",
+        channel="CC4A68V54",
+        text=trendingPosts()
+    )
 
 sched.start()
