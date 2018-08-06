d867c0f0c33380a401598c4ba81d1455387acd37
Alissa Castro (IS-PH) <alissa_castro@trendmicro.com>
2018-08-06 23:34:55 +0800

    Code modification

--- hello/views.py
+++ hello/views.py
@@ -5,16 +5,21 @@ from django.views.decorators.csrf import csrf_exempt
 from tweepy import OAuthHandler, API
 import json
 
+# oAuth_token = environ.get('oAuth_token', None)
+slack_aToken = slackclient.SlackClient(environ.get('oAuth_token', None))
+# bot_token = environ.get('bot_token', None)
+slack_bToken = slackclient.SlackClient(environ.get('bot_token', None))
+
+twitter_cKey = environ.get('consumer_key', None)
+twitter_sKey = environ.get('consumer_secret', None)
+twitter_aToken = environ.get('access_token', None)
+twitter_aSToken = environ.get('access_token_secret', None)
+
+auth = OAuthHandler(twitter_cKey, twitter_sKey)
+auth.set_access_token(twitter_aToken, twitter_aSToken)
+api = API(auth)
+
 def trendingPosts():
-    twitter_cKey = environ.get('consumer_key', None)
-    twitter_sKey = environ.get('consumer_secret', None)
-    twitter_aToken = environ.get('access_token', None)
-    twitter_aSToken = environ.get('access_token_secret', None)
-
-    auth = OAuthHandler(twitter_cKey, twitter_sKey)
-    auth.set_access_token(twitter_aToken, twitter_aSToken)
-    api = API(auth)
-
     WOE_ID = 1
     trending = api.trends_place(WOE_ID)
     trending = json.loads(json.dumps(trending, indent=1))
@@ -26,17 +31,11 @@ def trendingPosts():
 
     return trend_list
 
-
 def responseBot(request):
-    oAuth_token = environ.get('oAuth_token', None)
-    slack_aToken = slackclient.SlackClient(oAuth_token)
-    bot_token = environ.get('bot_token', None)
-    slack_bToken = slackclient.SlackClient(bot_token)
-
     channel_event = json.loads(request.body)
     get_eventChannel = channel_event["event"]["channel"]
     get_eventText = channel_event["event"]["text"]
-    if "top" in get_eventText:
+    if ("top" in get_eventText):
         slack_bToken.api_call(
             "chat.postMessage",
             channel=get_eventChannel,
@@ -49,7 +48,6 @@ def responseBot(request):
             text="Can't find trending posts. Sorry! Try again tomorrow :D"
         )
     return ""
-
 
 @csrf_exempt
 def accessUrl(request):
