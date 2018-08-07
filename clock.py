27d469e2e8f2a4a7dfe6748ada91a26bb2746e14
Alissa Castro (IS-PH) <alissa_castro@trendmicro.com>
2018-08-07 09:46:55 +0800

    Added a function that will check if Hiroku server is up

--- clock.py
+++ clock.py
@@ -1,3 +1,5 @@
+import os
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 from hello import views
 from hello.views import slack_bToken, trendingPosts
@@ -11,6 +13,15 @@ sched = BlockingScheduler()
 #         channel="CC316TH17",
 #         text=trendingPosts()
 #     )
+google = "google.com"
+ping_google = os.system("ping" + google)
+
+@sched.scheduled_job('interval', minutes=15)
+def timed_job():
+    if ping_google == 0:
+        print ("Server is awake")
+    else:
+        print ("Server is sleeping")
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=13)
 def scheduled_job():
