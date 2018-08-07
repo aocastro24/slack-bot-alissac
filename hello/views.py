3c8c78bdbe3b56dac7930a76225ce2a7ed23fe84
Alissa Castro (IS-PH) <alissa_castro@trendmicro.com>
2018-08-07 11:45:33 +0800

    Syntax Error fix

--- hello/views.py
+++ hello/views.py
@@ -24,7 +24,8 @@ def trendingPosts():
     WOE_ID = 1
     trending = api.trends_place(WOE_ID)
     trending = json.loads(json.dumps(trending, indent=1))
-    trend_list = [].sort()
+    trend_list = []
+    trend_list.sort()
     for trend in trending[0]["trends"]:
         trend_list.append((trend["name"]))
     trend_list = ', \n'.join(trend_list[:10])
